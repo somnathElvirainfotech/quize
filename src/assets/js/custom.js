f7a6dffb69e912ef50fb98eec0512311f364069b
somnathElvirainfotech <somnath.elvirainfotech@gmail.com>
2023-09-14 20:21:13 +0530

    fdgfdgf

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,27 +1,28 @@
 const jquery = require("jquery");
 const { default: Iframe } = require("react-iframe");
 
-jquery(document).ready(function($) {
+jquery(document).ready(function ($) {
     // hamburgers
-    var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+    var forEach = function (t, o, r) { if ("[object Object]" === Object.prototype.toString.call(t)) for (var c in t) Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t); else for (var e = 0, l = t.length; l > e; e++)o.call(r, t[e], e, t) };
     var hamburgers = document.querySelectorAll(".hamburger");
     if (hamburgers.length > 0) {
-    forEach(hamburgers, function(hamburger) { hamburger.addEventListener("click", function() {
-        this.classList.toggle("is-active");
-    }, false);
-    });  
-}
-// hamburgers
+        forEach(hamburgers, function (hamburger) {
+            hamburger.addEventListener("click", function () {
+                this.classList.toggle("is-active");
+            }, false);
+        });
+    }
+    // hamburgers
 
-//menu
+    //menu
     var pull = jquery('#pull');
     var menu = jquery('#menu-bg');
-    var menuHeight  = menu.height();
-    jquery(pull).on('click', function(e) {
-        alert(1)
-        e.preventDefault();
-        menu.slideToggle(500);
-    });
+    var menuHeight = menu.height();
+    // jquery(pull).on('click', function (e) {
+    //     alert(1)
+    //     e.preventDefault();
+    //     menu.slideToggle(500);
+    // });
     // jquery(window).resize(function(){
     //     alert(1)
     //     var w = jquery(window).width();
@@ -30,9 +31,10 @@ jquery(document).ready(function($) {
     // // }
     // });
 
-    // jquery(document).on('click', '.deletelanguage', function(){
-    //     alert("success");
-    // });
+    jquery(document).on('click', '#pull', function(){
+        alert("success");
+        menu.slideToggle(500);
+    });
 
 
 
